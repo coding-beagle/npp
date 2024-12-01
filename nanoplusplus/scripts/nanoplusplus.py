4863b3d8fac2cdd92c1d1f3a7637397038ffacc9
coding-beagle <nicholas.teague@focusrite.com>
2024-12-01 22:54:24 +0000

    it looks kinda good now though

--- nanoplusplus/scripts/nanoplusplus.py
+++ nanoplusplus/scripts/nanoplusplus.py
@@ -6,8 +6,14 @@ from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.layout.containers import HSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.layout.dimension import Dimension
+from prompt_toolkit.styles import Style
 
 kb = KeyBindings()
+
+style = Style([
+    ('top', 'bg:#5b29bc fg:#ffffff bold')
+])
 
 
 @kb.add('c-x')
@@ -20,23 +26,32 @@ def exit_(event):
 def cli(filename: str) -> int:
     """Open a file in the NanoPlusPlus text editor!"""
     # try:
+    if (filename is None):
+        filename = ""
     target = Path(filename)
-    if (target.exists()):
-        buffer1 = Buffer()
-        text_field = Window(content=BufferControl(buffer=buffer1))
 
-        root_container = HSplit([
-            Window(content=FormattedTextControl(text=f"Editing {filename}")),
-            text_field]
-        )
-        layout = Layout(root_container)
-        app = Application(layout=layout, full_screen=True, key_bindings=kb)
+    buffer1 = Buffer()
+    text_field = Window(content=BufferControl(buffer=buffer1))
 
+    if (target.exists() and filename != ""):
+        title = f"Editing {filename}"
         with open(target, "r") as fp:
             content = fp.read()
             buffer1.insert_text(content)
+    else:
+        title = f"New Buffer"
 
-        app.run()
+    root_container = HSplit([
+        Window(content=FormattedTextControl(text=title),
+               style='bg:#7b39cc fg:#ffffff bold',
+               height=Dimension(max=1)),
+        text_field]
+    )
+    layout = Layout(root_container)
+    app = Application(layout=layout, full_screen=True,
+                      key_bindings=kb, style=style)
+
+    app.run()
 
     # except Exception as e:
     #     print(e)
