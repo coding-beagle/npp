8b47a7ccace030ff39a15c83f268e77765621d70
coding-beagle <nicholas.teague@focusrite.com>
2024-12-02 00:10:51 +0000

    enhance README with installation instructions and refactor editor layout for improved usability

--- nanoplusplus/scripts/nanoplusplus.py
+++ nanoplusplus/scripts/nanoplusplus.py
@@ -5,6 +5,7 @@ from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.layout.containers import HSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
+from prompt_toolkit.layout import ScrollablePane, ScrollOffsets
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.layout.dimension import Dimension
 from prompt_toolkit.styles import Style
@@ -26,16 +27,21 @@ class Editor():
     app: Application
     layout: Layout
     root_container: HSplit
-    text_field: Window
+    text_window: Window
     title: str
+    pop_up_enabled: bool
 
     def __init__(self):
 
         self.title = ""
 
+        self.pop_up_enabled = False
+
         self.buffer1 = Buffer()
-        self.text_field = Window(content=BufferControl(
-            buffer=self.buffer1), height=Dimension(max=10))
+        self.text_window = Window(content=BufferControl(
+            buffer=self.buffer1))
+        self.text_scroll = ScrollablePane(
+            self.text_window, show_scrollbar=False)
 
         self.update_ui()
 
@@ -47,25 +53,48 @@ class Editor():
             Window(content=FormattedTextControl(text=self.title),
                    style='bg:#7b39cc fg:#ffffff bold',
                    height=Dimension(max=1)),
-            self.text_field]
+            self.text_scroll]
         )
         self.layout = Layout(self.root_container)
         self.app = Application(layout=self.layout, full_screen=True,
                                key_bindings=kb, style=style)
         self.app.invalidate()
 
-    def pop_up(self, str, colour, yes_cb, no_cb):
+    def pop_up(self, str, colour, yes_cb=None, no_cb=None):
+        if (self.pop_up_enabled):
+            return
+
         self.pop_up_enabled = True
 
         @kb.add('y', filter=is_pop_up)
         def yes_handler(buffer):
-            yes_cb()
+            if (yes_cb is not None):
+                yes_cb()
             self.pop_up_enabled = False
+            self.root_container = HSplit([
+                Window(content=FormattedTextControl(text=self.title),
+                       style='bg:#7b39cc fg:#ffffff bold',
+                       height=Dimension(max=1)),
+                self.text_scroll,],
+            )
+            self.layout = Layout(self.root_container)
+            self.app.layout = self.layout
+            self.app.invalidate()
 
         @kb.add('n', filter=is_pop_up)
         def no_handler(buffer):
-            no_cb()
+            if (no_cb is not None):
+                no_cb()
             self.pop_up_enabled = False
+            self.root_container = HSplit([
+                Window(content=FormattedTextControl(text=self.title),
+                       style='bg:#7b39cc fg:#ffffff bold',
+                       height=Dimension(max=1)),
+                self.text_scroll,],
+            )
+            self.layout = Layout(self.root_container)
+            self.app.layout = self.layout
+            self.app.invalidate()
 
         yes_no_window = Window(
             content=FormattedTextControl(text=str),
@@ -77,16 +106,16 @@ class Editor():
             Window(content=FormattedTextControl(text=self.title),
                    style='bg:#7b39cc fg:#ffffff bold',
                    height=Dimension(max=1)),
-            self.text_field,
+            self.text_scroll,
             yes_no_window],
         )
-        # self.update_ui()
         self.layout = Layout(self.root_container)
         self.app.layout = self.layout
         self.app.invalidate()
 
     def add_text_to_content(self, text):
         self.buffer1.insert_text(text)
+        self.buffer1.cursor_position = 0
 
     def set_title(self, title):
         self.title = title
@@ -97,15 +126,15 @@ class Editor():
         self.app.run()
 
 
-@ kb.add('c-x')
+@ kb.add('c-z')
 def exit_(event):
-    event.app.exit()
+    editor.pop_up("Exit from file? (y/n)", "#00ccff",
+                  yes_cb=event.app.exit)
 
 
 @ kb.add('c-o')
 def write_out_(event):
-    editor.pop_up("Write out file?", "#ffccff", yes_cb=lambda: print(
-        "yes"), no_cb=lambda: print("no"))
+    editor.pop_up("Write out file? (y/n)", "#ffccff", no_cb=event.app.exit)
 
 
 @ click.command()
